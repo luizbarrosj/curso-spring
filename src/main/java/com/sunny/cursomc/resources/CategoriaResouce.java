aa99ad40fea395c8d6b455ddace335fcc444afb8
Luiz Barros <luizbarro82@gmail.com>
2022-01-18 20:25:08 -0300

    Rest Testado

---
+++ src/main/java/com/sunny/cursomc/resources/CategoriaResouce.java
@@ -0,0 +1,22 @@
+package com.sunny.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResouce {
+	
+	
+	@GetMapping
+	public String listar() {
+		
+		return "Rest está Funcionando";
+		
+		
+	}
+	
+
+
+}
