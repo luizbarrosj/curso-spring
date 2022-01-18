16a0a21041b60d25b5adfec0fbdfe28d3ea3b6bb
Luiz Barros <luizbarro82@gmail.com>
2022-01-18 20:08:19 -0300

    Projeto Criado

---
+++ src/test/java/com/sunny/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
