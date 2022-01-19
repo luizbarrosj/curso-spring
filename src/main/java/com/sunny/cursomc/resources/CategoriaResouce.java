4fdc62df9f4cf5ed7d56f029e3db069ddd439a19
Luiz Barros <luizbarro82@gmail.com>
2022-01-19 19:37:13 -0300

    Testando a primerira classe de dominio

--- src/main/java/com/sunny/cursomc/resources/CategoriaResouce.java
+++ src/main/java/com/sunny/cursomc/resources/CategoriaResouce.java
@@ -1,8 +1,13 @@
 package com.sunny.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sunny.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,9 +15,16 @@ public class CategoriaResouce {
 	
 	
 	@GetMapping
-	public String listar() {
+	public List<Categoria>  listar() {
 		
-		return "Rest está Funcionando";
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista=new  ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 		
 		
 	}
